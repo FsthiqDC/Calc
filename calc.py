6a8966140bf6d8d7831cd57ad1ec179cc077cc5a
FsthiqDC <maciejkrysiakpz@gmail.com>
2021-05-26 01:58:12 +0200

    added rows and cols for buttons

--- calc.py
+++ calc.py
@@ -1,9 +1,25 @@
 import tkinter as tk
+from tkinter.constants import ANCHOR, BOTTOM
 
 root = tk.Tk()
 root.title("Calculator")
 root.geometry("220x280")
 root.resizable(False,False)
 
+root.columnconfigure(0, pad=3)
+root.columnconfigure(1, pad=3)
+root.columnconfigure(2, pad=3)
+root.columnconfigure(3, pad=3)
+
+root.rowconfigure(0, pad=3)
+root.rowconfigure(1, pad=3)
+root.rowconfigure(2, pad=3)
+root.rowconfigure(3, pad=3)
+root.rowconfigure(4, pad=3)
+
+#////
+
+
+mr.pack()
 
 root.mainloop()
