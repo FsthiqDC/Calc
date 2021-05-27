d0852ad7e077e0aa0c205bb9dcebf098d765587c
FsthiqDC <maciejkrysiakpz@gmail.com>
2021-05-28 01:43:42 +0200

    buttons properly puts value in input box

--- calc.py
+++ calc.py
@@ -13,6 +13,22 @@ def creating_window():
 
     return root
 
+def button_click(text_box, symbol):
+    def f():
+        if symbol == "\u21BA":
+             textLenght = len(text_box.get())
+             text_box.delete(textLenght-1, END)
+
+        elif symbol == "C":
+            text_box.delete(0, END)
+
+        else:
+            text = symbol if symbol != "x^2" else "^2"
+            text_box.insert(END, text)
+
+    return f
+
+
 def creating_buttons(root):
     buttons = [Button(root, text=symbol, bg="#5b94d9", borderwidth=0, width=4, height=2) for symbol in symbols]
 
@@ -21,26 +37,27 @@ def creating_buttons(root):
         if i % 6 == 0:
             j+=1
         buttons[i].grid(row=j, column=i%6,ipady=3, ipadx=6)
+        buttons[i].configure(command=button_click(text_box, buttons[i]["text"]))
 
     special = Button(root, text="=", bg="#228f82",borderwidth=0, width=10, height=2)
     special.grid(row = j, column=4, columnspan=2, ipady=3, ipadx=8)
 
     return buttons
 
-def user_input(root, buttons):
+def user_input(root):
     input_box = Entry(root, borderwidth=0, background="#ffffff")
     input_box.grid(row=2, columnspan=6, ipadx=76, ipady=25)
 
     result_box = Label(root, background="#ffffff")
     result_box.grid(row=1, columnspan=6, ipadx=134, ipady=30)
-
+    return input_box, result_box
 
 
 
 if __name__ == "__main__":
 
     root = creating_window()
+    text_box, info = user_input(root)
     buttons = creating_buttons(root)
-    text_box = user_input(root, buttons)
 
     root.mainloop()
