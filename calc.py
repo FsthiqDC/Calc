1d4e24f201059c71c05bc36d5ae1096d077384d0
FsthiqDC <maciejkrysiakpz@gmail.com>
2021-05-30 18:01:56 +0200

    Proper input handling

--- calc.py
+++ calc.py
@@ -3,7 +3,12 @@ import tkinter as tk
 from tkinter import *
 
 symbols = [
-    "7","8","9","/","\u21BA","C","4","5","6","*","(",")","1","2","3","-","x^2","\u221A","0", ",","%","+",]
+    "7","8","9","/","\u21BA","C","4","5","6","*","(",")","1","2","3","-","^2","\u221A","0", ",","%","+"
+    ]
+
+operators = [
+    "+", "^2", "*", "(", ")", "/", "%", ","
+]
 
 def creating_window():
     root = Tk()
@@ -13,18 +18,24 @@ def creating_window():
 
     return root
 
-def button_click(text_box, symbol):
+def button_click(symbol, input):
     def f():
         if symbol == "\u21BA":
-             textLenght = len(text_box.get())
-             text_box.delete(textLenght-1, END)
+            text_length = len(input.get())
+            input.delete(text_length-1, END)
 
         elif symbol == "C":
-            text_box.delete(0, END)
+            input.delete(0, END)
 
         else:
-            text = symbol if symbol != "x^2" else "^2"
-            text_box.insert(END, text)
+            help = len(input.get())
+            last_char = input.get()
+
+            if symbol in operators:
+                if symbol != last_char[help-1]:
+                    input.insert(END, symbol)
+            else:
+                input.insert(END, symbol)
 
     return f
 
@@ -37,27 +48,28 @@ def creating_buttons(root):
         if i % 6 == 0:
             j+=1
         buttons[i].grid(row=j, column=i%6,ipady=3, ipadx=6)
-        buttons[i].configure(command=button_click(text_box, buttons[i]["text"]))
+        buttons[i].configure(command=button_click(symbols[i], input))
 
     special = Button(root, text="=", bg="#228f82",borderwidth=0, width=10, height=2)
     special.grid(row = j, column=4, columnspan=2, ipady=3, ipadx=8)
 
     return buttons
 
-def user_input(root):
-    input_box = Entry(root, borderwidth=0, background="#ffffff")
-    input_box.grid(row=2, columnspan=6, ipadx=76, ipady=25)
+def  creating_input(root):
+    result_box = Label()
+    result_box.grid(row = 0, columnspan=6,ipadx=134, ipady=28)
 
-    result_box = Label(root, background="#ffffff")
-    result_box.grid(row=1, columnspan=6, ipadx=134, ipady=30)
-    return input_box, result_box
+    input_box = Entry()
+    input_box.grid(row = 1, columnspan=6, ipadx=75, ipady=28)
+
+    return result_box, input_box
 
 
 
 if __name__ == "__main__":
 
     root = creating_window()
-    text_box, info = user_input(root)
+    result, input = creating_input(root)
     buttons = creating_buttons(root)
 
     root.mainloop()
